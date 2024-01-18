00ffe5cdc038ced1c05d728306988c4a2756ecda
zhuxiujia@qq.com <zhuxiujia@qq.com>
2024-01-18 15:12:43 +0800

    add doc

--- src/sync/wg.rs
+++ src/sync/wg.rs
@@ -2,6 +2,42 @@ use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
 
 /// WaitGroup impl use channel,it's also both support sync and async
+/// how to use?
+///
+/// * on tokio
+/// ```rust
+/// use dark_std::sync::WaitGroup;
+/// use std::time::Duration;
+/// use tokio::time::sleep;
+/// #[tokio::main]
+/// async fn main() {
+///     let wg = WaitGroup::new();
+///     let wg2 = wg.clone();
+///     tokio::spawn(async move {
+///         sleep(Duration::from_secs(1)).await;
+///         drop(wg2);
+///     });
+///     wg.wait_async().await;
+///     println!("all done");
+/// }
+/// ```
+/// * on thread
+/// ```rust
+/// use dark_std::sync::WaitGroup;
+/// use std::time::Duration;
+/// use std::thread::sleep;
+///
+/// fn main() {
+///     let wg = WaitGroup::new();
+///     let wg2 = wg.clone();
+///     std::thread::spawn(move ||{
+///         sleep(Duration::from_secs(1));
+///         drop(wg2);
+///     });
+///     wg.wait();
+///     println!("all done");
+/// }
+/// ```
 pub struct WaitGroup {
     pub total: Arc<AtomicU64>,
     pub recv: Arc<flume::Receiver<u64>>,
