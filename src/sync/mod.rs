514e65e00d3bdd57c657149c54475024ab9ddf24
zxj <zhuxiujia@qq.com>
2024-05-03 17:56:55 +0800

    impl Vec get_mut owner lock

--- src/sync/mod.rs
+++ src/sync/mod.rs
@@ -1,7 +1,5 @@
 pub mod map_btree;
 pub mod map_hash;
-
-#[cfg(feature = "map_index")]
 pub mod map_index;
 pub mod vec;
 pub mod wg;
@@ -11,7 +9,6 @@ pub mod duration;
 pub use duration::*;
 pub use map_btree::SyncBtreeMap;
 pub use map_hash::SyncHashMap;
-#[cfg(feature = "map_index")]
 pub use map_index::SyncIndexMap;
 pub use vec::*;
 pub use wg::*;
